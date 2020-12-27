806996fe88fcbe5e1c48b465de86e2d8be4439ca
Yuta Saito <kateinoigakukun@gmail.com>
2020-12-27 10:48:33 +0900

    Refine config

--- Rocket/Common/Config.swift
+++ Rocket/Common/Config.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 protocol Config {
-    static var appClientId: String { get }
-    static var appClientSecret: String { get }
+    static var cognitoAppClientId: String { get }
+    static var cognitoAppClientSecret: String { get }
     static var cognitoCcopes: Set<String> { get }
     static var cognitoSignInRedirectUri: String { get }
     static var cognitoSignOutRedirectUri: String { get }
     static var cognitoWebDomain: String { get }
-    static var userPoolIdForEnablingASF: String { get }
+    static var cognitoIdentityPoolId: String { get }
+
     static var apiEndpoint: String { get }
     static var s3Bucket: String { get }
-    static var identityPoolId: String { get }
     static var youTubeApiKey: String { get }
 }
