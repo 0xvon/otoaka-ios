48b8bf859ca388552bf507cb7cb75e469f828ff0
masatsch <kingmasatojames@gmail.com>
2021-01-07 19:32:34 +0900

    loading indicator init

---
+++ Targets/UIComponent/Atom/LoadingCollectionView.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingCollectionView.swift
+//  Rocket
+//
+//  Created by Masato TSUTSUMI on 2021/01/07.
+//
+
+import UIKit
+
+public final class LoadingCollectionView: UIActivityIndicatorView {
+    public init() {
+        super.init(frame: .zero)
+        setup()
+    }
+    
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setup() {
+        backgroundColor = Brand.color(for: .background(.primary))
+        color = Brand.color(for: .text(.primary))
+    }
+}
+
+#if PREVIEW
+import SwiftUI
+
+struct LoadingCollectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewWrapper(view: LoadingCollectionView())
+                .previewLayout(.fixed(width: 320, height: 500))
+        }
+        .background(Color.black)
+    }
+}
+#endif
