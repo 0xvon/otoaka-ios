55568c21ce4e3e4ba6f1f3b692c2e0a32c4e839c
masatsch <kingofmasatojames@gmail.com>
2021-03-07 21:25:14 +0900

    todo: deep link

--- Targets/Rocket/AppDelegate.swift
+++ Targets/Rocket/AppDelegate.swift
@@ -35,6 +35,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        // TODO: Deep Link for custom URL schemes
+        return true
+    }
 
     func application(
         _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]
